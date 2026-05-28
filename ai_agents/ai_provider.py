0a091ca738e361a8894c71214435e079c0b39830
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-05-28 20:26:38 +0530

    Update ai_provider.py

--- ai_agents/ai_provider.py
+++ ai_agents/ai_provider.py
@@ -43,7 +43,7 @@ def ask_groq(prompt):
                 "content": prompt
             }
         ],
-        model="llama3-8b-8192"
+        model="llama-3.1-8b-instant"
     )
 
     return completion.choices[0].message.content
